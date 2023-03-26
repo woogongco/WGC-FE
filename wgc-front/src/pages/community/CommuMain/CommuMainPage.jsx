6b5182c1cbe1f7e60b5f4b31577be8d7bc0839e2
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-03-26 18:31:10 +0900

    [WOOG-53] feat: 커뮤니티 메인 페이지 레이아웃 및 라우터 연결

---
+++ wgc-front/src/pages/community/CommuMain/CommuMainPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CommuMainContainer from 'containers/community/CommuMain/CommuMainContainer';
+
+export default function CommuMainPage() {
+	return (
+		<>
+			{/* <Header /> */}
+			{/* <Profile /> */}
+			<CommuMainContainer />
+			{/* <Footer /> */}
+		</>
+	);
+}
