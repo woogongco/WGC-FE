8e458056f6070df89dd35199333cbe7676a0d2a9
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-04-06 00:56:23 +0900

    [WOOG-57] refactor: 협업 코드 맞추기 및 리팩토링

--- wgc-front/src/App.js
+++ wgc-front/src/App.js
@@ -7,7 +7,7 @@ import LoginMainPage from 'pages/login/LoginMainPage';
 import UserSignPage from 'pages/login/UserSignPage';
 import PwFindPage from 'pages/login/PwFindPage';
 import EmailLoginPage from 'pages/login/EmailLoginPage';
-import CommuMainPage from 'pages/community/CommuMain/CommuMainPage';
+import CommuMainPage from 'pages/community/CommuMainPage';
 import Profile from 'components/profile/Profile';
 
 function App() {
