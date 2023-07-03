e0ffcaaca09cc4a033c5668d20ec679bc5012a79
lovaoi777 <lovaoi777@naver.com>
2023-07-03 20:03:36 +0900

    [WOOG-105] design : 댓글 레이아웃

--- wgc-front/src/containers/mini/WriteModifyContainer.jsx
+++ wgc-front/src/containers/mini/WriteModifyContainer.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { AiOutlineLike } from 'react-icons/ai';
 import { BsBookmark } from 'react-icons/bs';
+import { FaThumbsUp } from 'react-icons/fa';
 import axios from 'axios';
 const Container = styled.div`
 	width: 100%;
@@ -124,12 +125,58 @@ const CategoryText = styled.div`
 	text-align: center;
 `;
 
-const CommnetDiv = styled.div`
-	width: 58%;
-	display: flex;
-	justify-content: flex-start;
+const CommentLayout = styled.div`
+	width: 85%;
 `;
+const CommentDiv = styled.div`
+	border: 1px solid #5e5e63;
+	border-radius: 5px;
+	margin-bottom: 20px;
+	padding: 10px;
+	width: 100%;
+	padding-left: 40px;
+`;
+const CommentName = styled.div`
+	font-size: 14px;
+	border-bottom: 1px solid #5e5e63;
+	display: flex;
+	justify-content: space-between;
+`;
+const CommentInput = styled.input`
+	background-color: transparent;
+	border: none;
+	color: white;
+	&:focus {
+		outline: none;
+	}
+`;
+const CommentContent = styled.div`
+	display: flex;
+	justify-content: space-between;
 
+	align-items: center;
+`;
+const CommentProfile = styled.div`
+	position: relative;
+	width: 50px;
+	height: 50px;
+	background: linear-gradient(219.11deg, #b9e6e9 30.15%, #cb8387 89.69%);
+	border-top-left-radius: 40%;
+	border-top-right-radius: 50%;
+	border-bottom-right-radius: 50%;
+	left: 30px;
+`;
+const Commentdetail = styled.div`
+	font-size: 13px;
+`;
+const CommentContainer = styled.div`
+	display: flex;
+`;
+const CommentMent = styled.div`
+	margin: 0 5px;
+	cursor: pointer;
+	font-size: 11px;
+`;
 export default function WriteModifyContainer() {
 	const [PostList, setPostList] = useState([]);
 	const [PostUser, setPostUser] = useState([]);
@@ -149,11 +196,7 @@ export default function WriteModifyContainer() {
 
 	useEffect(
 		e => {
-			// PostList['registerDate'] = PostList['registerDate']
-			// 	.replace(/-/gi, '.')
-			// 	.replace('T', ' ')
-			// 	.slice(0, PostList['registerDate'].length - 3);
-			console.log('!1111111', PostList);
+			// PostList['registerDate'] = PostList['registerDate'];
 		},
 		[PostList],
 	);
@@ -197,9 +240,44 @@ export default function WriteModifyContainer() {
 					</div>
 				</PostBottomDiv>
 			</PostDiv>
-			<CommnetDiv>
+			<CommentLayout>
 				<p>댓글</p>
-			</CommnetDiv>
+				<CommentContainer>
+					<CommentProfile />
+					<CommentDiv>
+						<CommentName>이름</CommentName>
+						<CommentInput placeholder="여기에 작성해주세요" />
+					</CommentDiv>
+				</CommentContainer>
+				<Comment />
+			</CommentLayout>
 		</Container>
+	);
+}
+
+export function Comment() {
+	return (
+		<div>
+			<CommentContainer>
+				<CommentProfile />
+				<CommentDiv>
+					<CommentName>
+						<div>작성자 이름</div>
+						<div> 시간</div>
+					</CommentName>
+					<CommentContent>
+						<Commentdetail>내용</Commentdetail>
+						<CommentContent>
+							<CommentMent>
+								<FaThumbsUp />
+							</CommentMent>
+							<CommentMent>수정</CommentMent>
+							<CommentMent>삭제</CommentMent>
+							<CommentMent>댓글</CommentMent>
+						</CommentContent>
+					</CommentContent>
+				</CommentDiv>
+			</CommentContainer>
+		</div>
 	);
 }
