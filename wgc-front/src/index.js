7396192dc8a1366a9a33bcd5223afafc9132e849
lovaoi777 <lovaoi777@naver.com>
2023-09-11 16:23:42 +0900

    [WOOG-110] delect : mockservice

--- wgc-front/src/index.js
+++ wgc-front/src/index.js
@@ -2,14 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { worker } from './mocks/browser';
 import { Provider } from 'react-redux';
 import store from 'store/store';
-
-//service worker
-if (process.env.NODE_ENV === 'development') {
-	worker.start();
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
