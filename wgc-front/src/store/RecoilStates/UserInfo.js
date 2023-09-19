82b71751686ad7e0c2cf142004bf0418398a2889
lovaoi777 <lovaoi777@naver.com>
2023-09-19 19:16:25 +0900

    feat : recoil 랜더링 방지

--- wgc-front/src/store/RecoilStates/UserInfo.js
+++ wgc-front/src/store/RecoilStates/UserInfo.js
@@ -1,6 +1,12 @@
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
 
+const { persistAtom } = recoilPersist({
+	key: 'localStorage', // 고유한 key 값
+	storage: localStorage,
+});
 export const myInfo = atom({
 	key: 'myInfo',
 	default: undefined,
+	effects_UNSTABLE: [persistAtom],
 });
