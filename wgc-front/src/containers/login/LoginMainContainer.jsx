a28626c2e3bfa840234701e8cb65e5f8b27ea3a8
lovaoi777 <lovaoi777@naver.com>
2023-11-06 15:43:23 +0900

    fix : 카카오로그인 버튼 변경

--- wgc-front/src/containers/login/LoginMainContainer.jsx
+++ wgc-front/src/containers/login/LoginMainContainer.jsx
@@ -47,63 +47,10 @@ const Btn = styled.img`
 	margin-top: 1em;
 	cursor: pointer;
 `;
-const ModalBackdrop = styled.div`
-	// Modal이 떴을 때의 배경을 깔아주는 CSS를 구현
-	z-index: 1; //위치지정 요소
-	position: fixed;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	background-color: rgba(0, 0, 0, 0.4);
-	border-radius: 10px;
-	top: 0;
-	left: 0;
-	right: 0;
-	bottom: 0;
-`;
-const SNSContainer = styled.div`
-	background-color: white;
-	width: 500px;
-	height: 500px;
-	border-radius: 10px;
-`;
-const DeletBtn = styled.div`
-	display: flex;
-	float: right;
-	margin: 10px;
-`;
-const KaKaoLoGo = styled.img`
-	width:150px;
-	height :200p;x
-`;
-const KaKaoMent = styled.p`
-	font-size: 20px;
-	text-align: center;
-`;
-const KaKaLoginBtn = styled.div`
-	width: 400px;
-	height: 50px;
-	border-radius: 10px;
-	background-color: #fae64d;
-	text-align: center;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	cursor: pointer;
-`;
-const ContainerContent = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	margin-top: 70px;
-`;
 
 export default function LoginMainContainer() {
 	const [openModal, setModal] = useState(false);
 	const navigate = useNavigate();
-	const openModalHander = useCallback(() => {
-		setModal(!openModal);
-	}, [openModal]);
 
 	return (
 		<Container>
@@ -128,29 +75,7 @@ export default function LoginMainContainer() {
 					>
 						<div style={{ paddingTop: '10px' }}>로그인</div>
 					</div>
-					<Btn src={KakaoButton} alt="카카오" onClick={openModalHander} />
-					{openModal ? (
-						<ModalBackdrop>
-							<SNSContainer>
-								<DeletBtn onClick={openModalHander}>
-									<AiOutlineClose style={{ color: 'black' }} />
-								</DeletBtn>
-								<ContainerContent>
-									<KaKaoLoGo
-										src="https://cdn.imweb.me/upload/S20210304872ba49a108a8/89a68d1e3674a.png"
-										alt="카카오사진"
-									/>
-									<KaKaoMent>
-										간편하게 로그인하고 <br /> 다양한서비스를 이용해보세요.
-									</KaKaoMent>
-									<KaKaLoginBtn className="kakao-login-button">카카오로그인</KaKaLoginBtn>
-									<p>다른 이메일로 시작하기</p>
-								</ContainerContent>
-							</SNSContainer>
-						</ModalBackdrop>
-					) : (
-						''
-					)}
+					<Btn src={KakaoButton} alt="카카오" className="kakao-login-button" />
 					<Btn src={GitButton} alt="깃허브" />
 					<Link to="/UserSign">
 						<Btn src={EmailButton} alt="이메일" />
