0641398fe7aa1ae71ee3742a2fa823cc5ea7a2ce
lovaoi777 <lovaoi777@naver.com>
2023-12-07 15:32:38 +0900

    feat : Profile Click TransForm

--- wgc-front/src/components/profile/Profile.jsx
+++ wgc-front/src/components/profile/Profile.jsx
@@ -1,9 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { FaPlus } from 'react-icons/fa';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { myInfo } from '../../store/RecoilStates/UserInfo';
-import { axiosPost } from 'Utils/AxiosUtils';
+import { axiosPost, axiosGet } from 'Utils/AxiosUtils';
+import { useLocation } from 'react-router-dom';
 
 const Base = styled.div`
 	width: 100%;
@@ -29,6 +30,8 @@ const ContainerDiv = styled.div`
 	display: flex;
 	justify-content: center;
 	align-items: center;
+
+	transition: all ease 1s;
 `;
 
 const Title = styled.div`
@@ -50,6 +53,9 @@ const TitleWappr = styled.div`
 const StackTextContainer = styled.div`
 	display: flex;
 	color: white;
+`;
+const SpaceDiv = styled.div`
+	padding: 5rem;
 `;
 
 const StackTextItem = styled.div`
@@ -83,7 +89,6 @@ const StackWapper = styled.div`
 	justify-content: space-between;
 	margin-left: 10px;
 `;
-
 const StackItem = styled.div`
 	background-color: ${props => props.color || 'red'};
 	padding: 5px;
@@ -135,9 +140,11 @@ const FristColorCircle = styled.div`
 	height: 19px;
 	background: #b3cad3;
 	position: absolute;
-	top: -5%;
-	right: 5%;
+	transition: all 0.5s linear;
+	top: -10%;
+	right: 0%;
 	border-radius: 360%;
+	${props => (props.onChangeTransForm ? 'transform: translate(-20px,10px)' : '')}
 `;
 
 const SecoundColorCircle = styled.div`
@@ -145,9 +152,11 @@ const SecoundColorCircle = styled.div`
 	height: 19px;
 	background: #ffb548;
 	position: absolute;
-	top: 5%;
-	right: -3%;
+	transition: all 0.5s linear;
+	top: -10%;
+	right: 0%;
 	border-radius: 360%;
+	${props => (props.onChangeTransForm ? 'transform: translate(0px,40px)' : '')}
 `;
 
 const ThreeColorCircle = styled.div`
@@ -155,83 +164,113 @@ const ThreeColorCircle = styled.div`
 	height: 19px;
 	background: #5a0cff;
 	position: absolute;
-	top: 20%;
-	right: -10%;
+	top: -10%;
+	right: 0%;
 	border-radius: 360%;
+	transition: all 0.5s linear;
+
+	${props => (props.onChangeTransForm ? 'transform: translate(20px,70px)' : '')}
 `;
 
 const FourColorCircle = styled.div`
 	width: 19px;
 	height: 19px;
-	background: #ffb548;
-	position: absolute;
-	top: 35%;
-	right: -13%;
-	border-radius: 360%;
-`;
-
-const FiveColorCircle = styled.div`
-	width: 19px;
-	height: 19px;
 	background: #ff2a38;
 	position: absolute;
-	top: 50%;
-	right: -13%;
+	transition: all 0.5s linear;
+	top: -10%;
+	right: 0%;
 	border-radius: 360%;
+
+	${props => (props.onChangeTransForm ? 'transform: translate(35px,105px)' : '')}
 `;
+
+const Friend = styled.div`
+	display: flex;
+	justify-content: flex-end;
+	margin-right: 2rem;
+`;
+const FriendBtn = styled.button`
+	padding: 0.5rem;
+	border-radius: 0.5rem;
+	background-color: transparent;
+	color: white;
+	border: 0.1rem solid grey;
+	cursor: pointer;
+`;
+
 //fetch로 값이 있을 경우 00님의 미니홈피 없다면 null 값으로 보여주고
 //자기소개 작성과 today 일촌 관리
 //색깔변하는 작은 원들은 백엔드에서 색깔 데이터를 받아와서 원을 만들예쩡
 // 배열을 만들어서 해당 index 번호를 가지고 맞으면 그 색깔을 보여주는 logic으로 생각중
 // store에 있는 값을 가져올수도 있다.
 export default function Profile() {
-	const userInfo = useRecoilValue(myInfo);
-
-	const HandleColor = useCallback(async color => {
-		const res = await axiosPost('/member/information', {
-			color: color,
-		});
-		console.log(res);
+	const [userInfo, setUserInfo] = useRecoilState(myInfo);
+	const [TransForm, setTransForm] = useState(true);
+	const location = useLocation();
+	const HandleColor = useCallback(
+		async color => {
+			await axiosPost('/member/information', {
+				color: color,
+			});
+			setUserInfo(prevUserInfo => ({
+				...prevUserInfo,
+				color: color,
+			}));
+		},
+		[setUserInfo],
+	);
+	const onClickTransForm = useCallback(() => {
+		setTransForm(prev => !prev);
 	}, []);
-
 	return (
 		<Base>
-			<Title>
-				<TitleWappr>{userInfo && <h4>{userInfo.name}님의 미니 홈피</h4>}</TitleWappr>
-			</Title>
-			{userInfo && ( // FIXME : 사용자 소개 스타일링 필요함.
+			{location.pathname === '/Guest' ||
+			location.pathname === '/Friend' ||
+			location.pathname === '/Minimain' ? (
 				<>
+					<Title>
+						<TitleWappr>{<h4>{userInfo.name}님의 미니 홈피</h4>}</TitleWappr>
+					</Title>
 					<br />
 					<h5 style={{ color: 'white' }}>{userInfo.introduction}</h5>
 				</>
+			) : (
+				<Title>
+					<TitleWappr>{<h4>우리 모두 코딩해요!</h4>}</TitleWappr>
+				</Title>
 			)}
-			<ContainerDiv style={{ backgroundImage: `url(${userInfo.profileImage || ''})` }}>
+			<ContainerDiv
+				onClick={onClickTransForm}
+				style={{ backgroundImage: `url(${userInfo.profileImage || ''})` }}
+			>
 				{!userInfo && <div>로그인해주세요</div>}
-				<FristColorCircle
-					onClick={e => {
-						HandleColor('#b3cad3');
-					}}
-				/>
-				<SecoundColorCircle
-					onClick={e => {
-						HandleColor('#ffb548');
-					}}
-				/>
-				<ThreeColorCircle
-					onClick={e => {
-						HandleColor('#5a0cff');
-					}}
-				/>
-				<FourColorCircle
-					onClick={e => {
-						HandleColor('#ffb548');
-					}}
-				/>
-				<FiveColorCircle
-					onClick={e => {
-						HandleColor('#ff2a38');
-					}}
-				/>
+				<div>
+					<FristColorCircle
+						onChangeTransForm={TransForm}
+						onClick={e => {
+							HandleColor('#b3cad3');
+						}}
+					/>
+					<SecoundColorCircle
+						onChangeTransForm={TransForm}
+						onClick={e => {
+							HandleColor('#ffb548');
+						}}
+					/>
+					<ThreeColorCircle
+						onChangeTransForm={TransForm}
+						onClick={e => {
+							HandleColor('#5a0cff');
+						}}
+					/>
+					<FourColorCircle
+						onChangeTransForm={TransForm}
+						onClick={e => {
+							HandleColor('#ff2a38');
+						}}
+					/>
+				</div>
 			</ContainerDiv>
 			<StackTextContainer>
 				<StackTextItem>
@@ -257,6 +296,9 @@ export default function Profile() {
 				</span>
 				<WgcTextBottomP>커뮤니티부터 프로젝트 준비까지</WgcTextBottomP>
 				<Borderdiv />
+				<Friend>
+					<FriendBtn>일촌신청</FriendBtn>
+				</Friend>
 			</WoogCoTextContainer>
 		</Base>
 	);
